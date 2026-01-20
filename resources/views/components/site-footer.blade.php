7a002bf6922d1673a83959ac0b62f12cff5c7b37
Geky <gekymedia@gmail.com>
2026-01-20 11:58:29 +0000

    Update footer contacts and address

--- resources/views/components/site-footer.blade.php
+++ resources/views/components/site-footer.blade.php
@@ -1,5 +1,5 @@
 @php
-$phone = ['+233 552964461', '+233 244847555', '+233 200669653'];
+$phone = ['+233 200669653'];
 @endphp
 
 <footer class="bg-dark text-white py-8 md:py-12 border-t w-full">
@@ -90,7 +90,7 @@ $phone = ['+233 552964461', '+233 244847555', '+233 200669653'];
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"></path>
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path>
                             </svg>
-                            <span>Oduom Bus Stop,Kumasi, Ashanti Region, Ghana.</span>
+                            <span>Apemso, Pentecost Junction, Kumasi</span>
                         </li>
                         @foreach($phone as $number)
                             <li>
